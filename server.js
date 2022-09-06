eb6e5311286b1f0177c18aae2b58a2dfe751f1e4
verve-neowise <amanbayevu@gmail.com>
2022-09-06 15:14:39 +0500

    connect prisma

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const cors = require('cors')
 
+const { PrismaClient } = require('@prisma/client')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const users = []
+const prisma = new PrismaClient()
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.json(users)
 })
 
-app.post('/users', (req, res) => {
-    users.push(req.body)
+app.post('/users', async (req, res) => {
+    
+    const user = await prisma.user.create({
+        data: {
+            name: req.body.name,
+            surname: req.body.surname
+        }
+    })
+
     res.json({
         message: "User created.",
-        user: req.body
+        user
     })
 })
 
